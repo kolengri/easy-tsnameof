30a9ae2c85401adb2d2750165f53029d7bd051ff
kolengri <kolenko@me.com>
2021-01-07 20:49:04 +0100

    Add possibility to replace indexes

--- src/nameOf.ts
+++ src/nameOf.ts
@@ -5,7 +5,11 @@ export type NameOfPath<T> = NameOfPathFunc<T> | keyof T;
 
 export type NameOf<T> = (f: NameOfPath<T>, deep: number) => string;
 
-export const nameOf = <T>(f: NameOfPath<T>, deep = 0): string => {
+export const nameOf = <T, Replace extends object = any>(
+  f: NameOfPath<T>,
+  deep = 0,
+  replace?: Replace
+): string => {
   if (typeof f === 'string') {
     return f;
   }
@@ -17,7 +21,15 @@ export const nameOf = <T>(f: NameOfPath<T>, deep = 0): string => {
     .replace(/ /g, '')
     .replace(/\s/g, '')
     .split('.');
-  const result = arr.splice(deep + 1).join('.');
+  let result = arr.splice(deep + 1).join('.');
+
+  if (replace) {
+    result = Object.entries(replace).reduce((res, cur) => {
+      const [key, val] = cur;
+      return res.replace(key, val);
+    }, result);
+  }
+
   return result;
 };
 
--- test/nameOf.ts
+++ test/nameOf.ts
@@ -5,6 +5,7 @@ type TestObject1 = {
     test2: {
       test: string;
     };
+    test3: { test4: number }[];
   };
 };
 
@@ -18,5 +19,16 @@ describe('TS NameOf', () => {
     const name = nameOf<TestObject1>((o) => o.test1.test2.test, 1);
 
     expect(name).toEqual('test2.test');
+  });
+
+  it('Test nameOf replace', async () => {
+    const index = 999;
+    const name = nameOf<TestObject1, { index: number }>(
+      (o) => o.test1.test3[index].test4,
+      0,
+      { index }
+    );
+
+    expect(name).toEqual('test1.test3[999].test4');
   });
 });
