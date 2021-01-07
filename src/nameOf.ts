9ca4ae5ff6ce10c4fc03a08f9efc081bb08bf33c
kolengri <kolenko@me.com>
2021-01-07 21:05:19 +0100

    Update types

--- src/nameOf.ts
+++ src/nameOf.ts
@@ -3,13 +3,24 @@ import { DeepRequired } from 'ts-essentials';
 export type NameOfPathFunc<T> = (obj: DeepRequired<T> | never) => unknown;
 export type NameOfPath<T> = NameOfPathFunc<T> | keyof T;
 
-export type NameOf<T> = (f: NameOfPath<T>, deep: number) => string;
+export type ReplaceObject = {
+  [key: string]: number | string;
+};
 
-export const nameOf = <T, Replace extends object = any>(
-  f: NameOfPath<T>,
-  deep = 0,
-  replace?: Replace
-): string => {
+export type NameOf = {
+  <PathObject, Replace extends ReplaceObject = ReplaceObject>(
+    f: NameOfPath<PathObject>,
+    deep?: number,
+    replace?: Replace
+  ): string;
+
+  <PathObject, Replace extends ReplaceObject = ReplaceObject>(
+    f: NameOfPath<PathObject>,
+    replace?: Replace
+  ): string;
+};
+
+export const nameOf: NameOf = (f: NameOfPath<any>, ...otherArgs: any[]) => {
   if (typeof f === 'string') {
     return f;
   }
@@ -21,19 +32,36 @@ export const nameOf = <T, Replace extends object = any>(
     .replace(/ /g, '')
     .replace(/\s/g, '')
     .split('.');
+
+  let deep = 0;
+  let replace: ReplaceObject | undefined = undefined;
+
+  if (typeof otherArgs[0] === 'number') {
+    deep = otherArgs[0];
+  }
+
+  if (typeof otherArgs[0] === 'object') {
+    replace = otherArgs[0];
+  } else if (typeof otherArgs[1] === 'object') {
+    replace = otherArgs[1];
+  }
+
   let result = arr.splice(deep + 1).join('.');
 
   if (replace) {
     result = Object.entries(replace).reduce((res, cur) => {
       const [key, val] = cur;
-      return res.replace(key, val);
+      return res.replace(key, String(val));
     }, result);
   }
 
   return result;
 };
 
-export const nameOfFabric = <T>() => (f: NameOfPath<T>, deep = 0) =>
-  nameOf<T>(f, deep);
+export const nameOfFabric = <
+  T,
+  Replace extends ReplaceObject = ReplaceObject
+>() => (f: NameOfPath<T>, deep = 0, replace: Replace) =>
+  nameOf<T>(f, deep, replace);
 
 export default nameOf;
